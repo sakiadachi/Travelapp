350181c6aa4ef492a8daa1d88fb72302ff43ca2d
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2020-04-12 23:48:35 +0900

    Some changes after the feedback
    add cors
    add service worker
    *hover state

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
+import './styles/reset.scss'
 import './styles/style.scss'
 import './styles/header.scss'
 import './styles/form.scss'
-import './styles/footer.scss'
 import './styles/entry.scss'
 
 import { handleSubmit } from './js/formHandler'
